e7413fed46b27574655ce0be692fb4a8287e1256
Syahroni12 <ronixria@gmail.com>
2025-05-13 20:53:21 +0700

    first commit

---
+++ database/migrations/2025_05_13_133824_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['on_going', 'in_progress', 'completed'])->default('on_going');
+            $table->date('dari_tgl')->nullable();
+            $table->date('sampai_tgl')->nullable();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
